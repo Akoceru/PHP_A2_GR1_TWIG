733c564fdc3593528754eb77088a126e56cdb36e
axel <axl.78@hotmail.fr>
2014-10-25 17:27:04 +0200

    End project

--- index.php
+++ index.php
@@ -4,23 +4,34 @@
  * This code is under MIT licence (see https://github.com/Irvyne/license/blob/master/MIT.md)
  */
 
+require __DIR__.'/vendor/autoload.php';
 require __DIR__.'/_header.php';
 
-//$perPage = 6; // nbArticleParPage
-//$nbArticles = countArticles($link); //nbArticleTotal
-//$currentPage = !empty($_GET['p']) ? (int)$_GET['p'] : 1;// numéro de la page
-//$nbPages = ceil($nbArticles/$perPage); // nombre de pagination
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem([
+    __DIR__.'/views',
+]);
 
 
-//if (0 >= $currentPage) {
-    //header('Location: index.php?p=1');
-//}
-//if ($currentPage > $nbPages) {
-   // header('Location: index.php?p='.$nbPages);
-//}
 
-//$articles = getArticles($link, null, ($currentPage-1)*$perPage, $perPage);
+$perPage = 6;
+$total = countArticles($link);
+$currentPage = !empty($_GET['p']) ? (int)$_GET['p'] : 0;
+$lastPage = (int)floor($total/$perPage);
 
+
+
+
+if (0 >= $currentPage) {
+    header('Location: articles.php?p=1');
+}
+if ($currentPage > ($lastPage+1)) {
+    header('Location: articles.php?p='.($lastPage+1));
+}
+$articles = getArticles($link, null, ($currentPage-1)*$perPage, $perPage);
+$twig = new Twig_Environment($loader,[
+//'cache' => null,
+]);
 $homeUsername = [
     'getUsername' => getSession(),
 ];
@@ -31,13 +42,14 @@ $homeConnected = isConnected();
 
 $homeSession = $_SESSION;
 
-
-echo $twig->render('base.html.twig', [
+echo $twig->render('articles.html.twig', [
+    'articles' => $articles,
+    'perPage' => $perPage,
+    'total' => $total,
+    'currentPage' => $currentPage,
+    'lastPage' => $lastPage,
     'homeUsername' => $homeUsername,
     'homeAdmin' => $homeAdmin,
     'homeConnected' => $homeConnected,
     'homeSession' => $homeSession,
 ]);
-
-
-
