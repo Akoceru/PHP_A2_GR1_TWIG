a85e45252e6490f55cdf608304e995fbbe63f246
axel <axl.78@hotmail.fr>
2014-10-26 15:32:45 +0100

    End project for real bro' ya know

--- admin-article-list.php
+++ admin-article-list.php
@@ -4,10 +4,36 @@
  * This code is under MIT licence (see https://github.com/Irvyne/license/blob/master/MIT.md)
  */
 
-require __DIR__.'/_header-admin.php';
+
+require __DIR__.'/vendor/autoload.php';
+require __DIR__.'/_header.php';
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem([
+    __DIR__.'/views',
+]);
 
 $articles = getArticles($link);
+$twig = new Twig_Environment($loader,[
+//'cache' => null,
+]);
+$homeUsername = [
+    'getUsername' => getSession(),
+];
+$homeAdmin = [
+    'isAdmin' => isAdmin(),
+];
+$homeConnected = isConnected();
 
-include __DIR__.'/template/admin-article-list.php';
+$homeSession = $_SESSION;
 
-require __DIR__.'/_footer.php';
+
+
+echo $twig->render('admin-article-list.html.twig', [
+    'articles' => $articles,
+    'homeUsername' => $homeUsername,
+    'homeAdmin' => $homeAdmin,
+    'homeConnected' => $homeConnected,
+    'homeSession' => $homeSession,
+
+]);
+
--- login.php
+++ login.php
@@ -3,8 +3,14 @@
  * @author Thibaud BARDIN (https://github.com/Irvyne).
  * This code is under MIT licence (see https://github.com/Irvyne/license/blob/master/MIT.md)
  */
-
+require __DIR__.'/vendor/autoload.php';
 require __DIR__.'/_header.php';
+
+
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem([
+    __DIR__.'/views',
+]);
 
 if (isConnected()) {
     header('Location: index.php');
@@ -16,16 +22,83 @@ if (isset($_POST['loginSubmit'])) {
 
     if (empty($username) || empty($password)) {
         $missing_credential = true;
+
+        $twig = new Twig_Environment($loader,[
+//'cache' => null,
+        ]);
+        $homeUsername = [
+            'getUsername' => getSession(),
+        ];
+        $homeAdmin = [
+            'isAdmin' => isAdmin(),
+        ];
+        $homeConnected = isConnected();
+
+        $homeSession = $_SESSION;
+
+        echo $twig->render('login.html.twig', [
+
+
+            'missing_credential' => $missing_credential,
+            'homeUsername' => $homeUsername,
+            'homeAdmin' => $homeAdmin,
+            'homeConnected' => $homeConnected,
+            'homeSession' => $homeSession,
+        ]);
     } else {
         $connection = connection($link, $username, $password);
         if ($connection) {
             header('Location: index.php');
         } else {
             $credential_error = true;
+
+            $twig = new Twig_Environment($loader,[
+//'cache' => null,
+            ]);
+            $homeUsername = [
+                'getUsername' => getSession(),
+            ];
+            $homeAdmin = [
+                'isAdmin' => isAdmin(),
+            ];
+            $homeConnected = isConnected();
+
+            $homeSession = $_SESSION;
+
+            echo $twig->render('login.html.twig', [
+
+                'credential_error' => $credential_error,
+
+                'homeUsername' => $homeUsername,
+                'homeAdmin' => $homeAdmin,
+                'homeConnected' => $homeConnected,
+                'homeSession' => $homeSession,
+            ]);
         }
     }
 }
 
-include __DIR__.'/template/login.php';
+else{
 
-require __DIR__.'/_footer.php';
+    $twig = new Twig_Environment($loader,[
+//'cache' => null,
+    ]);
+    $homeUsername = [
+        'getUsername' => getSession(),
+    ];
+    $homeAdmin = [
+        'isAdmin' => isAdmin(),
+    ];
+    $homeConnected = isConnected();
+
+    $homeSession = $_SESSION;
+
+    echo $twig->render('login.html.twig', [
+
+        'homeUsername' => $homeUsername,
+        'homeAdmin' => $homeAdmin,
+        'homeConnected' => $homeConnected,
+        'homeSession' => $homeSession,
+    ]);
+}
+var_dump($_POST);
